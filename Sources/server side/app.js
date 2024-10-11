ff217ae8adddacaf6d4f4511fa7c783006f734fb
metabuilders <amirhoseinehaddadi@gmail.com>
2024-10-11 23:07:36 +0330

    builing database stuff for signup page

--- Sources/server side/app.js
+++ Sources/server side/app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const sqlite3 = require('sqlite3').verbose();
-let sql;
+const sqlite3 = require('sqlite3').verbose()
+const bodyParser = require('body-parser');
+//db
 
-const db = new sqlite3.Database('./../../users data/users-data.db' , (err)=>{
-    if(err) console.error(err.message);
+
+const db = new sqlite3.Database("./../../users data/users-data.db" ,(err)=>{
+    if(err){
+        console.error(err.message);
+    }
 })
 
-sql = "";
-
-
 app.use(express.static(path.join(__dirname , "../../")));
+app.use(bodyParser.json());
 
+app.post("/api-add-new-user" , (req , res)=>{
+    info = req.body
+    if(info.password == info.passwordConfirmation){
+        db.run(`INSERT INTO users(email,password) VALUES(?,?)` , [info.email , info.password] , (err)=>{
+            console.error(err)
+        })
+        return res.status(200).send()
+    }
+    else{
+        alert("signup ERROR : password and passowrd confirmation has difrences")
+    }
+}
+)
 app.get('/', (req , res)=>{
     res.sendFile("index.html");
 })
